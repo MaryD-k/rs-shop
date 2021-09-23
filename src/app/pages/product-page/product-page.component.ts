61914c8db424b5acd41c3f3cba02358055ebcb77
MaryD-k <maryia.dyliuk@gmail.com>
2021-09-23 13:36:40 +0300

    feat: add description block for product page

--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Category } from '@core/models/category.model';
+import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product.model';
+import { getUser } from 'src/app/redux/user/user.actions';
+import { CartHttpService } from 'src/app/services/cart-http.service';
 import { CategoriesHttpService } from 'src/app/services/categories-http.service';
 import { GoodsHttpService } from 'src/app/services/goods-http.service';
 
@@ -32,7 +35,9 @@ export class ProductPageComponent implements OnInit {
 
   constructor(private activatedRoute: ActivatedRoute,
     private categoriesHttpService: CategoriesHttpService,
-    private goodsHttpService: GoodsHttpService) {
+    private goodsHttpService: GoodsHttpService,
+    private cartHttpService: CartHttpService,
+    private store: Store) {
     this.subscription = activatedRoute.params.subscribe(params => {
       this.currentSubcategory = params.categoryName;
       this.productId = params.productId;
@@ -50,12 +55,34 @@ export class ProductPageComponent implements OnInit {
         }
       }
     });
-    this.goodsHttpService.getProductById(this.productId).subscribe(product => this.product = product);
+    this.goodsHttpService.getProductById(this.productId).subscribe(product => {
+      this.product = product;
+      this.checkLSCart();
+    });
     this.selectedPhotoIndex = 0;
   }
 
   switchPhoto(index: number) {
     this.selectedPhotoIndex = index;
+  }
+
+  addToBasket(product: Product) {
+    let currentUserToken = localStorage.getItem('token');
+    this.cartHttpService.addItemToCart(product.id, currentUserToken!).subscribe(() => {},
+      error => {
+        this.cartHttpService.addItemToLocalStorage(product.id);
+      }
+    );
+    this.product.isInCart = true;
+  }
+
+  checkLSCart() {
+    if(localStorage.getItem('cart')) {
+      let currentGoods: string[] = JSON.parse(localStorage.getItem('cart')!);
+      if(currentGoods.includes(this.product.id)) {
+        this.product.isInCart = true;
+      }
+    }
   }
 
 }
